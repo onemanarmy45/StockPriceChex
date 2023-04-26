3ecb1f0e1a7650a3a87ff2fd9e8d200dbc982361
Robert J <106358887+onemanarmy45@users.noreply.github.com>
2023-04-26 11:16:33 -0400

    final px

---
+++ StockPriceChex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+var client = new HttpClient();
+var request = new HttpRequestMessage
+
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://realstonks.p.rapidapi.com/msft"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", "c0cef5ee96msh2108c722f3fe6dap18231cjsn05d6ed2c81b7" },
+        { "X-RapidAPI-Host", "realstonks.p.rapidapi.com" },
+    },
+};
+
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+}
