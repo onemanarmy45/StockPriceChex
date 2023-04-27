307385e442c200941e807594309436398db24d02
Robert J <106358887+onemanarmy45@users.noreply.github.com>
2023-04-27 14:20:04 -0400

    final px

--- StockPriceChex/StockInfo.cs
+++ StockPriceChex/StockInfo.cs
@@ -16,34 +16,6 @@ namespace StockPriceChex
 
             var tickerSymbol = Console.ReadLine();
 
-            //var client = new HttpClient();
-
-            //var stockURL = $"https://realstonks.p.rapidapi.com/{tickerSymbol}";
-
-            //var response = client.GetStringAsync(stockURL).Result;
-
-            //var stockData = JObject.Parse(response.Content).GetValue("price").ToString();
-
-            //Console.WriteLine(stockData);
-
-            //        var client = new HttpClient();
-            //        var request = new HttpRequestMessage
-            //        {
-            //            Method = HttpMethod.Get,
-            //            RequestUri = new Uri("https://realstonks.p.rapidapi.com/TSLA"),
-            //            Headers =
-            //{
-            //    { "X-RapidAPI-Key", "c0cef5ee96msh2108c722f3fe6dap18231cjsn05d6ed2c81b7" },
-            //    { "X-RapidAPI-Host", "realstonks.p.rapidapi.com" },
-            //},
-            //        };
-            //        using (var response = await client.SendAsync(request))
-            //        {
-            //            response.EnsureSuccessStatusCode();
-            //            var body = await response.Content.ReadAsStringAsync();
-            //            Console.WriteLine(body);
-            //        }
-
             var client = new RestClient($"https://realstonks.p.rapidapi.com/{tickerSymbol}");
             var request = new RestRequest();
             request.AddHeader("content-type", "application/octet-stream");
@@ -55,4 +27,3 @@ namespace StockPriceChex
         }
 	}
 }
-
